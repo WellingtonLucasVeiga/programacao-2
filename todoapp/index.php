7dd270ef8908cc4240ff7c6f55488f3276972418
Jonathan Suter <jonathan.vinicius.suter@gmail.com>
2025-08-06 07:50:58 -0300

    Inicial

---
+++ todoapp/index.php
@@ -0,0 +1,45 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulário</title>
+</head>
+<body>
+    <?php
+        function Salvar($todo){
+            $connection = require("dbfactory.php");                        
+            if ($connection -> 
+                query(@"INSERT INTO todo (description) VALUES ('$todo');")) {                 
+            }
+            $connection -> close();
+        }
+        function Recuperar(){
+            $connection = require("dbfactory.php");
+            $sql = "SELECT idtodo, description FROM todo";
+
+            $result = $mysqli->query($sql);
+            echo "<table>";
+            while ($row = $result->fetch_assoc()) {            
+                echo "<tr>" 
+                        . "<td>".$row["idtodo"]."</td>"
+                        . "<td>".$row["description"]."</td>"
+                    ."</tr>";
+            }
+            echo "</table>";
+        }
+
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            $description = htmlspecialchars($_POST['description']); 
+            if(!empty($description)){
+                Salvar($description);
+            }           
+        }
+        Recuperar();        
+    ?>
+    <form method="post">
+        <label for="todo-description">Descrição da tarefa:</label>
+        <input name="description" id="todo-description" type="text">
+        <button type="submit">Enviar</button>
+    </form> 
+</body>
+</html>
